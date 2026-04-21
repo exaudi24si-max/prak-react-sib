a59a468af9bb57d2f20bc9d31e758ff0a2d1b8c7
exaudi24si-max <exaudi24si@mahasiswa.pcr.ac.id>
2026-04-21 14:57:47 +0700

    Menerapkan Beberapa fitur Router: Instalasi dam Link

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,10 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
+
+export default function Customers() {
+  return (
+    <div id="dashboard-container">
+        <PageHeader/>
+        <p>Ini halaman Cutomers</p>
+    </div>
+  );
+}
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,10 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
+
+export default function Orders() {
+  return (
+    <div id="dashboard-container">
+        <PageHeader/>
+        <p>Ini halaman Orders</p>
+    </div>
+  );
+}
